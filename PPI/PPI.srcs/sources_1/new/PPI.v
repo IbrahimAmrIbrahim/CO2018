235450c385bcb3bfb59cf7bcb3bfde0a20b7e142
ahmed hessuin <ahmed_1k997@yahoo.com>
2018-11-24 00:58:22 +0200

    no message

--- PPI/PPI.srcs/sources_1/new/PPI.v
+++ PPI/PPI.srcs/sources_1/new/PPI.v
@@ -1,8 +1,32 @@
 `timescale 1ns / 1ps
 
-module ReadWriteControlLogic(A , RDbar , WRbar , Reset);
+module ReadWriteControlLogic(A , RDbar , WRbar , Reset,CSbar);
+input A[1:0];
+input RDbar ;
+input WRbar ;   
+input Reset ;
+input CSbar;
 
 endmodule
+
+
+module Acontroler(order,out_to_A_port,out_to_C_upper_port);
+input order;
+output out_to_A_port;
+output out_to_C_upper_port;
+endmodule
+
+
+
+module Bcontroler(order,out_to_B_port,out_to_C_lower_port);
+input order;
+output out_to_B_port;
+output out_to_C_lower_port;
+endmodule
+
+
+module
+
 
 module PPI(D , PA , PB , PC , A , RDbar , WRbar , CSbar , Reset);
 inout [7:0] D , PA , PB , PC;
