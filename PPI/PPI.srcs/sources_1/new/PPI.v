faa6772e2a704f2ff09a99c5e8128538c00d6495
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2018-11-23 23:56:16 +0200

    Define chip.

--- PPI/PPI.srcs/sources_1/new/PPI.v
+++ PPI/PPI.srcs/sources_1/new/PPI.v
@@ -1,4 +1,8 @@
 `timescale 1ns / 1ps
 
-module PPI();
+module PPI(D , PA , PB , PC , A , RDbar , WRbar , CSbar , Reset );
+inout [7:0] D , PA , PB , PC;
+input [1:0] A;
+inout RDbar , WRbar , CSbar , Reset;
+
 endmodule
