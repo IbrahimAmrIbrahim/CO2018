3860a0ad187c4cbbc53afb349b35cf159a9b80b2
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2018-11-24 00:38:07 +0200

    Conroller

--- PPI/PPI.srcs/sources_1/new/PPI.v
+++ PPI/PPI.srcs/sources_1/new/PPI.v
@@ -1,8 +1,15 @@
 `timescale 1ns / 1ps
 
-module PPI(D , PA , PB , PC , A , RDbar , WRbar , CSbar , Reset );
+module ReadWriteControlLogic(A , RDbar , WRbar , Reset);
+
+endmodule
+
+module PPI(D , PA , PB , PC , A , RDbar , WRbar , CSbar , Reset);
 inout [7:0] D , PA , PB , PC;
 input [1:0] A;
-inout RDbar , WRbar , CSbar , Reset;
+input RDbar , WRbar , CSbar , Reset;
+
+assign PA = (~CSbar && Reset)? 8'b zzzz_zzzz : 8'b 1111_1111;
+
 
 endmodule
