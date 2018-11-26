407c6abaf896178edb7a24b30363639c962373a2
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2018-11-26 21:49:15 +0200

    no message

--- PPI/PPI.srcs/sources_1/new/PPI.v
+++ PPI/PPI.srcs/sources_1/new/PPI.v
@@ -1,24 +1,24 @@
 `timescale 1ns / 1ps
-module PortA(DataBus,A,InOut,Sel);
+module PortA(DataBus,A,Sel,RD,RW,Reset,CS);
 inout [7:0] A;
 inout [7:0] DataBus;
-input InOut;
 input [1:0] Sel;
+input RD,RW,Reset,CS ;
 
 /*
 InOut = 0 --> Input
 InOut = 1 --> Output
 */
 
-assign DataBus = (Sel == 2'b00)? ((InOut)? 8'bzzzz_zzzz : A) : 8'bzzzz_zzzz;
-assign A = (InOut)? ((Sel == 2'b00)? DataBus : A) : 8'bzzzz_zzzz;
+/*assign DataBus = (Sel == 2'b00)? ((InOut[0])? 8'bzzzz_zzzz : A) : 8'bzzzz_zzzz;
+assign A = (InOut[0])? ((Sel == 2'b00)? DataBus : A) : 8'bzzzz_zzzz;*/
 
 endmodule
 
 module PortB(DataBus,B,InOut,Sel);
 inout [7:0] B;
 inout [7:0] DataBus;
-input InOut;
+input [1:0] InOut;
 input [1:0] Sel;
 
 /*
@@ -26,15 +26,15 @@ InOut = 0 --> Input
 InOut = 1 --> Output
 */
 
-assign DataBus = (Sel == 2'b01)? ((InOut)? 8'bzzzz_zzzz : B) : 8'bzzzz_zzzz;
-assign B = (InOut)? ((Sel == 2'b01)? DataBus : B) : 8'bzzzz_zzzz;
+assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : B) : 8'bzzzz_zzzz;
+assign B = (InOut[1])? ((Sel == 2'b01)? DataBus : B) : 8'bzzzz_zzzz;
 
 endmodule
 
-module PortUC(DataBus,UC,InOut,Sel);
-inout [3:0] UC;
+module PortC(DataBus,C,InOut,Sel);
+inout [7:0] C;
 inout [7:0] DataBus;
-input InOut;
+input [1:0] InOut;
 input [1:0] Sel;
 
 /*
@@ -42,24 +42,8 @@ InOut = 0 --> Input
 InOut = 1 --> Output
 */
 
-assign DataBus = (Sel == 2'b10)? ((InOut)? 4'bzzzz : UC) : 4'bzzzz;
-assign UC = (InOut)? ((Sel == 2'b10)? DataBus : UC) : 4'bzzzz;
-
-endmodule
-
-module PortLC(DataBus,LC,InOut,Sel);
-inout [3:0] LC;
-inout [7:0] DataBus;
-input InOut;
-input [1:0] Sel;
-
-/*
-InOut = 0 --> Input
-InOut = 1 --> Output
-*/
-
-assign DataBus = (Sel == 2'b10)? ((InOut)? 4'bzzzz : LC) : 4'bzzzz;
-assign LC = (InOut)? ((Sel == 2'b10)? DataBus : LC) : 4'bzzzz;
+assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : C) : 8'bzzzz_zzzz;
+assign C = (InOut[1])? ((Sel == 2'b01)? DataBus : C) : 8'bzzzz_zzzz;
 
 endmodule
 
@@ -78,30 +62,9 @@ assign D = (RW)? 8'b zzzz_zzzz : DataBus;
 
 endmodule
 
-module ReadWriteControlLogic();
-
-endmodule
-
-
-module GroupAcontroler(PortA_ctrl , PortUC_ctrl , ctrl , DataBus);
-output PortA_ctrl , PortUC_ctrl;
-input [7:0] DataBus;
-input [2:0] ctrl;
-
-
-endmodule
-
-
-
-module GroupBcontroler();
-
-endmodule
-
-
 module PPI();
 
 endmodule
-
 
 module tb_PortA();
 reg RW , Sel;
