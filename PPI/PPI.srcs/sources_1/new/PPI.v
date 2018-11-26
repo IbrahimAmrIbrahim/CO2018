42f6dd447a07f21532e0b6e8eda61ad1c8737898
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2018-11-26 21:54:13 +0200

    no message

--- PPI/PPI.srcs/sources_1/new/PPI.v
+++ PPI/PPI.srcs/sources_1/new/PPI.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
-module PortA(DataBus,A,Sel,RD,RW,Reset,CS);
+module PortA(DataBus,A,Sel,RD_bar,RW_bar,Reset,CS_bar);
 inout [7:0] A;
 inout [7:0] DataBus;
 input [1:0] Sel;
-input RD,RW,Reset,CS ;
+input RD_bar,RW_bar,Reset,CS_bar ;
 
 /*
 InOut = 0 --> Input
@@ -15,50 +15,49 @@ assign A = (InOut[0])? ((Sel == 2'b00)? DataBus : A) : 8'bzzzz_zzzz;*/
 
 endmodule
 
-module PortB(DataBus,B,InOut,Sel);
+module PortB(DataBus,B,Sel,RD_bar,RW_bar,Reset,CS_bar);
 inout [7:0] B;
 inout [7:0] DataBus;
-input [1:0] InOut;
 input [1:0] Sel;
+input RD_bar,RW_bar,Reset,CS_bar ;
 
 /*
 InOut = 0 --> Input
 InOut = 1 --> Output
 */
 
-assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : B) : 8'bzzzz_zzzz;
-assign B = (InOut[1])? ((Sel == 2'b01)? DataBus : B) : 8'bzzzz_zzzz;
+/*assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : B) : 8'bzzzz_zzzz;
+assign B = (InOut[1])? ((Sel == 2'b01)? DataBus : B) : 8'bzzzz_zzzz;*/
 
 endmodule
 
-module PortC(DataBus,C,InOut,Sel);
+module PortC(DataBus,C,Sel,RD_bar,RW_bar,Reset,CS_bar);
 inout [7:0] C;
 inout [7:0] DataBus;
-input [1:0] InOut;
 input [1:0] Sel;
-
+input RD_bar,RW_bar,Reset,CS_bar ;
 /*
 InOut = 0 --> Input
 InOut = 1 --> Output
 */
 
-assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : C) : 8'bzzzz_zzzz;
-assign C = (InOut[1])? ((Sel == 2'b01)? DataBus : C) : 8'bzzzz_zzzz;
+/*assign DataBus = (Sel == 2'b01)? ((InOut[1])? 8'bzzzz_zzzz : C) : 8'bzzzz_zzzz;
+assign C = (InOut[1])? ((Sel == 2'b01)? DataBus : C) : 8'bzzzz_zzzz;*/
 
 endmodule
 
-module DataBusBuffer(DataBus,D,RW);
+module DataBusBuffer(DataBus,D,RD_bar,RW_bar,Reset,CS_bar);
 inout [7:0] D;
 inout [7:0] DataBus;
-input RW;
+input RD_bar,RW_bar,Reset,CS_bar ;
 
 /*
 Read  :RW = 0 , D Output , DataBus Input
 Write :RW = 1 , D Input , DataBus Output
 */
 
-assign DataBus = (RW)? D : 8'b zzzz_ZZZZ;
-assign D = (RW)? 8'b zzzz_zzzz : DataBus;
+/*assign DataBus = (RW)? D : 8'b zzzz_ZZZZ;
+assign D = (RW)? 8'b zzzz_zzzz : DataBus;*/
 
 endmodule
 
