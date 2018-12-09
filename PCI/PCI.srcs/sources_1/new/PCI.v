8fab2ec686cb32daa37fddaa413f738a28b98169
Almonzer-Alaaeldin <bronzemonzer1@gmail.com>
2018-12-09 12:56:52 +0200

    Change File PCI.v

--- PCI/PCI.srcs/sources_1/new/PCI.v
+++ PCI/PCI.srcs/sources_1/new/PCI.v
@@ -1,6 +1,13 @@
 `timescale 1ns / 1ps
 
-module device();
+module device(CLK, RST_N, AD, CBE_N, FRAME_N, IRDY_N, TRDY_N, DEVSEL_N, REQ_N, GNT_N);
+
+input CLK, RST_N, GNT_N;
+inout [31:0] AD;
+inout [3:0] CBE_N;
+inout FRAME_N, IRDY_N, TRDY_N, DEVSEL_N;
+output GNT_N;
+
 endmodule
 
 module arbiter_priority();
