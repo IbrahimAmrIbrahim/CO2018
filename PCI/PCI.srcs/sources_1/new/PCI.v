a64e4e745f0246b52b9fba8637d234b1cb0087cb
Almonzer-Alaaeldin <bronzemonzer1@gmail.com>
2018-12-11 17:45:55 +0200

    Changed File PCI.v

--- PCI/PCI.srcs/sources_1/new/PCI.v
+++ PCI/PCI.srcs/sources_1/new/PCI.v
@@ -1,32 +1,56 @@
 `timescale 1ns / 1ps
 
-module device_A(CLK, RST_N, AD, CBE_N, FRAME_N, IRDY_N, TRDY_N, DEVSEL_N, REQ_N, GNT_N);
+module device_A(CLK, RST_N, AD, CBE_N, FRAME_N, IRDY_N, TRDY_N, DEVSEL_N, REQ_N, GNT_N, FORCED_REQ_N, FORCED_ADDRESS, FORCED_CBE_N);
 
 parameter MyAddress = 32'hAA, TargetAddress = 32'hBC;
 
-input CLK, RST_N, GNT_N;
-inout [31:0] AD;	reg [31:0] ADreg;
+input CLK, RST_N, GNT_N, FORCED_REQ_N, FORCED_ADDRESS, FORCED_CBE_N;
+inout [31:0] AD;	reg [31:0] ADreg;  
 inout [3:0] CBE_N;	reg [3:0] CBE_Nreg;
 inout FRAME_N, IRDY_N, TRDY_N, DEVSEL_N; 
-reg REQ_Nreg = 1,FRAME_Nreg, IRDY_Nreg, TRDY_Nreg, DEVSEL_Nreg;
+reg FRAME_Nreg, IRDY_Nreg, TRDY_Nreg, DEVSEL_Nreg;
 output REQ_N;
 reg [31:0] memory [9:0];
+reg MasterFlag = 0, GNTFlag = 0;
+assign REQ_N = FORCED_REQ_N;
 
-assign AD = ADreg;	assign REQ_N = REQ_Nreg;	assign CBE_N = CBE_Nreg;
+assign AD = ADreg;	          	assign CBE_N = CBE_Nreg;
 assign FRAME_N = FRAME_Nreg;	assign IRDY_N = IRDY_Nreg;
-assign TRDY_N = TRDY_Nreg;	assign DEVSEL_N = DEVSEL_Nreg;
+assign TRDY_N = TRDY_Nreg;	    assign DEVSEL_N = DEVSEL_Nreg;
 
 always @ (posedge CLK)
-	
 	begin 
-		while (!GNT_N)
-		begin
-			#1 FRAME_Nreg <= 0; ADreg <= TargetAddress; CBE_Nreg <= 4'b1111;
-		end
-	end
+        if(!REQ_N) //Then Device is Master
+            MasterFlag = 1;
+
+        if (MasterFlag)
+            if(!GNT_N)
+                GNTFlag = 1;
+            else
+                begin 
+                    GNTFlag = 0;
+                    MasterFlag = 0;
+                end
+        
+        if (GNTFlag)
+            begin
+                FRAME_Nreg <= 0;
+                ADreg    <= FORCED_ADDRESS;
+                CBE_Nreg <= 4'b0110;
+            end
+
+        if(!FRAME_N)
+                IRDY_Nreg = 0;
+        if(!IRDY_N)
+            begin
+                ADreg    <= {(32){1'bz}};
+                CBE_Nreg <= 4'b1111;
+
+            end
+        
+    end
 
 endmodule
-
 module arbiter_priority(GNT_Neg , REQ_Neg , FRAME_Neg ,clk, RST_Neg);
 output reg [7:0] GNT_Neg;
 input [7:0] REQ_Neg;
