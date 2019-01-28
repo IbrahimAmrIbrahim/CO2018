569ae74122c961226b9806486ebfde6344ffa7d1
Abanob Medhat <abanobmedhat42@gmail.com>
2019-01-29 00:33:26 +0200

    delete some files

--- MIPS Processor/MIPS Processor.srcs/bob source/comips1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/Faculty/WorkSpace/Xillinx/comips1/tb_alu_isim_beh.exe" -prj "D:/Faculty/WorkSpace/Xillinx/comips1/tb_alu_beh.prj" "work.tb_alu" "work.glbl" 
