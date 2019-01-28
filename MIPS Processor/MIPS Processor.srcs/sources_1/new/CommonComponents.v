1f957f9ba178ae42bd8a022b79b0d3f8f865b48b
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2019-01-28 13:38:48 +0200

    Create MIPS project files.

---
+++ MIPS Processor/MIPS Processor.srcs/sources_1/new/CommonComponents.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module InstructionMemory();
+
+endmodule
+
+
+module DataMemory();
+
+endmodule
+
+
+module RegisterFile();
+
+endmodule
+
+
+module ALU();
+
+endmodule
+
+
+module ALUControl();
+
+endmodule
+
+
+module ControlUnit();
+
+endmodule
