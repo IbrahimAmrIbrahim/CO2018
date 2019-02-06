f7aabd5c22b722fe8e5129f064dbed1776cbeead
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2019-02-06 22:24:35 +0200

    MIPS pipeline.

--- MIPS Processor/MIPS Processor.srcs/sources_1/new/CommonComponents.v
+++ MIPS Processor/MIPS Processor.srcs/sources_1/new/CommonComponents.v
@@ -1,60 +1,124 @@
 `timescale 1ns / 1ps
 
-module InstructionMemory();
+module InstructionMemory(Instruction,PC,clk,RST,load,Data);
+input [31:0] PC;
+input clk,load,RST;
+output reg [31:0] Instruction;
+reg [7:0] memory [0:524287];
+input [31:0] Data;
+reg [31:0] loadAddress;
 
-endmodule
-
-
-module DataMemory(READ_DATA,ADDRESS,WRITE_DATA,MEM_WRITE,MEM_READ);
-input [31:0]ADDRESS,WRITE_DATA;
-input MEM_READ,MEM_WRITE;
-output reg [31:0] READ_DATA;
-reg [7:0] memory [0:4294967295];
-
-always@(*)
+always @(negedge clk)
 begin
-if(MEM_READ)
-    begin
-	READ_DATA[7:0]   <= memory[ADDRESS];
-	READ_DATA[15:8]  <= memory[ADDRESS+1];
-	READ_DATA[23:16] <= memory[ADDRESS+2];
-	READ_DATA[32:24] <= memory[ADDRESS+3];
-    end
-    
- if(MEM_WRITE)
-    begin
-    memory[ADDRESS]   <= WRITE_DATA[7:0];
-    memory[ADDRESS+1] <= WRITE_DATA[15:8];
-    memory[ADDRESS+2] <= WRITE_DATA[23:16];
-    memory[ADDRESS+3] <= WRITE_DATA[32:24];
-    end
+	if(load && RST)
+	begin
+		memory[loadAddress] <= Data[7:0];
+		memory[loadAddress + 1] <= Data[15:8];
+		memory[loadAddress + 2] <= Data[23:16];
+		memory[loadAddress + 3] <= Data[31:24];
+		loadAddress <= loadAddress + 4;
+	end
+end
+
+always @(posedge RST)
+begin
+	loadAddress <= 32'h0000_0000;
+end
+
+always @(PC)
+begin
+	Instruction[7:0] <= memory[PC];
+	Instruction[15:8] <= memory[PC + 1];
+	Instruction[23:16] <= memory[PC + 2];
+	Instruction[31:24] <= memory[PC + 3];
 end
 endmodule
 
 
-module RegisterFile(READ_DATA2,READ_DATA1,READ_REGISTER1,READ_REGISTER2,WRITE_REGISTER,WRITE_DATA,REG_WRITE);
+
+module DataMemory(READ_DATA,ADDRESS,WRITE_DATA,MEM_WRITE,MEM_READ,clk,RST,LOAD,DATA);
+input [31:0]ADDRESS,WRITE_DATA;
+input MEM_READ,MEM_WRITE,LOAD,clk,RST;
+output reg [31:0] READ_DATA;
+reg [7:0] memory [0:4294967295];
+reg [31:0] loadAddress;
+input [7:0] DATA;
+
+always@(negedge clk)
+begin
+	if(LOAD && RST)
+	begin
+		memory[loadAddress] <= DATA;
+		loadAddress <= loadAddress + 1;
+	end
+	else if(MEM_WRITE)
+	begin
+		memory[ADDRESS]   <= WRITE_DATA[7:0];
+		memory[ADDRESS+1] <= WRITE_DATA[15:8];
+		memory[ADDRESS+2] <= WRITE_DATA[23:16];
+		memory[ADDRESS+3] <= WRITE_DATA[31:24];
+	end
+end
+
+always @(posedge RST)
+begin
+	loadAddress <=32'h0000_0000;
+end
+
+always@(posedge clk)
+begin
+	if(~(LOAD && RST))
+	begin
+		if(MEM_READ)
+		begin
+		READ_DATA[7:0]   <= memory[ADDRESS];
+		READ_DATA[15:8]  <= memory[ADDRESS+1];
+		READ_DATA[23:16] <= memory[ADDRESS+2];
+		READ_DATA[31:24] <= memory[ADDRESS+3];
+		end
+	end
+end
+endmodule
+
+
+module RegisterFile(READ_DATA1,READ_DATA2,READ_REGISTER1,READ_REGISTER2,WRITE_REGISTER,WRITE_DATA,REG_WRITE,clk,RST,load,Data);
 input [4:0] READ_REGISTER2,READ_REGISTER1,WRITE_REGISTER;
-input REG_WRITE;
-input [31:0] WRITE_DATA;
+input REG_WRITE,load,clk,RST;
+input [31:0] WRITE_DATA,Data;
 output [31:0] READ_DATA2,READ_DATA1;
-reg [31:0] memory [31:0];
+reg [31:0] memory [0:31];
+reg [4:0] loadAddress;
 
 assign READ_DATA2 = memory[READ_REGISTER2];
 assign READ_DATA1 = memory[READ_REGISTER1];
 
-always@(*)
+always@(negedge clk)
 begin
-if(REG_WRITE)
-    begin
-    memory[WRITE_REGISTER] <= WRITE_DATA;
-    end
+	if(load && RST)
+	begin
+		memory[loadAddress] <= Data;
+		loadAddress <= loadAddress + 1;
+	end
+end
+
+always @(posedge RST)
+begin
+	loadAddress <= 5'b00000;
+end
+
+always@(posedge clk)
+begin
+	if(REG_WRITE)
+	begin
+		memory[WRITE_REGISTER] <= WRITE_DATA;
+	end
 end
 endmodule
 
 
 module mux4x1_32bit(in1,in2,in3,in4,sel,out);
 input [31:0] in1,in2,in3,in4;
-input [1:0]sel;
+input [1:0] sel;
 output reg [31:0] out;
 
 always@(*)
@@ -99,31 +163,124 @@ output zero;
 assign zero = (out == 32'h0000_0000)?1:0;
 always@(*)
 begin
-case(ALUCtrl)
-	4'b0000: out <= in1&in2;
-	4'b0001: out <= in1|in2;
-	4'b0010: out <= in1+in2;
-	4'b0110: out <= in1-in2;
-	4'b0111: out <= (in1<in2)? 32'h0000_0001:32'h0000_0000;
-endcase
+	case(ALUCtrl)
+		4'b0000: out <= in1&in2;
+		4'b0001: out <= in1|in2;
+		4'b0010: out <= in1+in2;
+		4'b0110: out <= in1-in2;
+		4'b0111: out <= (in1<in2)? 32'h0000_0001:32'h0000_0000;
+		4'b1100: out <= ~(in1|in2);
+	endcase
 end
 endmodule
 
 
-module ShiftLeft2(in1,out);
-input [31:0] in1;
+module ShiftLeft2(in,out);
+input [31:0] in;
 output [31:0] out;
 
-assign out = in1 << 2;
+assign out = in << 2;
 endmodule
 
 
-module ALUControl();
+module ALUControl(ALUOP,Func,ALUCtrl);
+input [1:0] ALUOP;
+input [5:0] Func;
+output reg [4:0] ALUCtrl;
 
+always @(*)
+begin
+	case(ALUOP)
+		2'b00: ALUCtrl <= 4'b0010;
+		2'b01: ALUCtrl <= 4'b0110;
+		2'b10: begin
+			case(Func)
+				6'b100000: ALUCtrl <= 4'b0010;
+				6'b100010: ALUCtrl <= 4'b0110;
+				6'b100100: ALUCtrl <= 4'b0000;
+				6'b100101: ALUCtrl <= 4'b0001;
+				6'b101010: ALUCtrl <= 4'b0111;
+				6'b100111: ALUCtrl <= 4'b1100;
+			endcase			
+		end
+	endcase
+end
 endmodule
 
 
-module ControlUnit();
+module ControlUnit(RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,OpCode);
+input [5:0] OpCode;
+output reg RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch;
+output reg [1:0] ALUOp;
+
+always @(*)
+begin
+	case(OpCode)
+		//R-Type
+		6'b000000:begin
+			RegDst <= 1'b1;
+			ALUSrc <= 1'b0;
+			MemtoReg <= 1'b0;
+			RegWrite <= 1'b1;
+			MemRead <= 1'b0;
+			MemWrite <= 1'b0;
+			Branch <= 1'b0;
+			ALUOp <= 2'b10;
+		end
+		//LW
+		6'b100011:begin
+			RegDst <= 1'b0;
+			ALUSrc <= 1'b1;
+			MemtoReg <= 1'b1;
+			RegWrite <= 1'b1;
+			MemRead <= 1'b1;
+			MemWrite <= 1'b0;
+			Branch <= 1'b0;
+			ALUOp <= 2'b00;
+		end
+		//SW
+		6'b101011:begin
+			RegDst <= 1'b0;
+			ALUSrc <= 1'b1;
+			MemtoReg <= 1'b0;
+			RegWrite <= 1'b0;
+			MemRead <= 1'b0;
+			MemWrite <= 1'b1;
+			Branch <= 1'b0;
+			ALUOp <= 2'b00;
+		end
+		//beq
+		6'b000100:begin
+			RegDst <= 1'b0;
+			ALUSrc <= 1'b0;
+			MemtoReg <= 1'b0;
+			RegWrite <= 1'b0;
+			MemRead <= 1'b0;
+			MemWrite <= 1'b0;
+			Branch <= 1'b1;
+			ALUOp <= 2'b01;
+		end
+		//slti
+		6'b001010:begin
+			RegDst <= 1'b0;
+			ALUSrc <= 1'b1;
+			MemtoReg <= 1'b0;
+			RegWrite <= 1'b1;
+			MemRead <= 1'b0;
+			MemWrite <= 1'b0;
+			Branch <= 1'b0;
+			ALUOp <= 2'b11;
+		end
+	endcase
+end
+endmodule
+
+
+module SignExtend(In,Out);
+input [15:0] In;
+output [31:0] Out;
+
+assign Out = {{16{In[15]}},In};
 
 endmodule
 
@@ -170,63 +327,61 @@ end
 endmodule
 
 
-module IF_ID(CLK,INPUT,OUTPUT,HOLD);
+module IF_ID__MEM(CLK,INPUT,OUTPUT);
 input [63:0]INPUT;
 output reg [63:0]OUTPUT;
-input CLK,HOLD;
+input CLK;
 reg [63:0]memory;
 
 always@(posedge CLK)
 begin
-	if(~HOLD)
-		memory <= INPUT;
+	OUTPUT <= memory;
 end
 	
 always@(negedge CLK)
 begin
-	if(~HOLD)
-		OUTPUT <= memory;
+	memory <= INPUT;
 end
 endmodule
 
 
-module ID_EX(CLK,INPUT,OUTPUT);
-input [119:0]INPUT;
-output reg [119:0]OUTPUT;
+module ID_EX__MEM(CLK,INPUT,OUTPUT);
+input [151:0]INPUT;
+output reg [151:0]OUTPUT;
 input CLK;
-reg [119:0]memory;
+reg [151:0]memory;
 
 always@(posedge CLK)
 begin
-	memory <= INPUT;
+	OUTPUT <= memory;	
 end
 
 always@(negedge CLK)
 begin
-	OUTPUT <= memory;
+	memory <= INPUT;
 end
 endmodule
 
 
-module EX_MEM(CLK,INPUT,OUTPUT);
-input [73:0]INPUT;
-output reg [73:0]OUTPUT;
+module EX_MEM__MEM(CLK,INPUT,OUTPUT);
+input [106:0]INPUT;
+output reg [106:0]OUTPUT;
 input CLK;
-reg [73:0]memory;
+reg [106:0]memory;
 
 always@(posedge CLK)
 begin
-	memory <= INPUT;
+	OUTPUT <= memory;
 end
 
 always@(negedge CLK)
 begin
-	OUTPUT <= memory;
+	memory <= INPUT;
 end
 endmodule
 
 
-module MEM_WB(CLK,INPUT,OUTPUT);
+module MEM_WB__MEM(CLK,INPUT,OUTPUT);
 input [70:0]INPUT;
 output reg [70:0]OUTPUT;
 input CLK;
@@ -234,12 +389,12 @@ reg [70:0]memory;
 
 always@(posedge CLK)
 begin
-	memory <= INPUT;
+	OUTPUT <= memory;
 end
 
 always@(negedge CLK)
 begin
-	OUTPUT <= memory;
+	memory <= INPUT;	
 end
 endmodule
 
@@ -367,4 +522,18 @@ RD_MEM=5;
 end
 
 forwarding_unit ahmed(RS,RT,RD_EX,RD_MEM,WB_EX,WB_MEM,MUX_RS,MUX_RT); 
+endmodule
+
+module tb_SignExtend();
+reg [15:0] In;
+wire [31:0] Out;
+
+initial
+begin
+In = 16'h0145;
+#10
+In = 16'hf475;
+end
+
+SignExtend Sign(In,Out);
 endmodule
---
+++ MIPS Processor/MIPS Processor.srcs/sources_1/new/PipelineMIPShazard.v
@@ -0,0 +1,331 @@
+`timescale 1ns / 1ps
+
+module FetchStageHAZARD(IF_ID,clk,PCSrc,BranchAddress,RST,load,Data,IF_ID_WRITE);
+input clk , PCSrc ,load,RST;
+input [31:0] BranchAddress,Data;
+reg [31:0] PC;
+output reg [63:0] IF_ID;
+wire [31:0] Instruction;
+input IF_ID_WRITE;
+always @(posedge clk,RST)
+begin
+	if (RST)
+	begin
+		PC <= 32'h0000_0000;
+	end
+	
+end
+
+always @(negedge clk)
+begin
+	if(~IF_ID_WRITE)
+	begin
+		IF_ID [31:0] <= Instruction;
+		IF_ID [63:32] <= PC;
+		if (~RST)
+		   begin
+				if (PCSrc)
+				begin
+					PC <= BranchAddress;
+				end
+				else
+				begin
+					PC <= PC + 4;
+				end
+		   end 
+	end	   
+end
+
+InstructionMemory InstructionMemory1(Instruction,PC,clk,RST,load,Data);
+endmodule
+
+module DecodeStageHAZARD(ID_EX,IF_ID,clk,WRITE_REGISTER,WRITE_DATA,REG_WRITE,RST,load,Data,IF_ID_WRITE,ID_EX_MEMORY_CONTROL,ID_EX_RT);
+output reg [151:0] ID_EX;
+wire RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch;
+wire [1:0] ALUOp;
+wire [31:0] SignExtend_wires;
+wire [31:0] READ_DATA1,READ_DATA2;
+input [4:0] WRITE_REGISTER;
+input [31:0] WRITE_DATA,Data;
+input [63:0] IF_ID;
+input REG_WRITE,clk,RST,load;
+input [4:0]ID_EX_RT;
+input ID_EX_MEMORY_CONTROL;
+wire MUX_0_CONTROL;
+output IF_ID_WRITE;
+always @(negedge clk)
+begin
+	ID_EX [4:0] <= IF_ID[15:11];// rd
+	ID_EX [9:5] <= IF_ID[20:16];//rt
+	ID_EX [14:10] <= IF_ID[25:21];//rs
+	ID_EX [46:15] <= SignExtend_wires;// 15:0 from instruction
+	ID_EX [78:47] <= READ_DATA2;
+	ID_EX [110:79] <= READ_DATA1;
+	ID_EX [142:111] <= IF_ID[63:32];// pc + 4
+		if(MUX_0_CONTROL==0)
+		begin
+		ID_EX [143] <= ALUSrc;
+		ID_EX [145:144] <= ALUOp;
+		ID_EX [146] <= RegDst;
+		ID_EX [147] <= MemWrite;
+		ID_EX [148] <= MemRead;
+		ID_EX [149] <= Branch;
+		ID_EX [150] <= MemtoReg;
+		ID_EX [151] <= RegWrite;
+		end
+		else 
+		begin
+		ID_EX [151:143] <= 0;
+		end
+		
+		
+		
+end
+hazard_detction_unit(ID_EX_MEMORY_CONTROL,IF_ID[25:21],IF_ID[20:16],ID_EX_RT,IF_ID_WRITE,MUX_0_CONTROL);
+SignExtend SignExtend1(IF_ID[15:0],SignExtend_wires);
+RegisterFile RegisterFile1 (READ_DATA2,READ_DATA1,IF_ID[25:21],IF_ID[20:16],WRITE_REGISTER,WRITE_DATA,REG_WRITE,clk,RST,load,Data);
+ControlUnit ControlUnit1(RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,IF_ID[31:26]);
+endmodule
+
+
+module ExecutionStageHAZARD(EX_MEM,clk,ID_EX,WB_EX,WB_MEM,RD_EX,RD_MEM,ALU_OUT,WRITE_BACK_DATA);
+output reg [106:0] EX_MEM;
+input WB_EX,WB_MEM; 
+input [4:0]RD_EX,RD_MEM;
+input [31:0]ALU_OUT,WRITE_BACK_DATA;
+wire [1:0]MUX_RS,MUX_RT;
+wire[31:0]mux1_out;
+wire[31:0]mux2_out;
+
+//reg write 
+input clk;
+input [151:0] ID_EX;
+wire [3:0]ALUCtrl;
+wire Zero_wire;
+wire [31:0] ALUOut;
+wire [31:0] Shifted_wire;
+
+always@(negedge clk)
+begin
+	EX_MEM [4:0] <= (ID_EX[146)? ID_EX[4:0]:ID_EX[9:5];
+	EX_MEM [36:5] <= ID_EX[78:47];
+	EX_MEM [68:37] <= ALUOut;
+	EX_MEM [69] <= Zero_wire;
+	EX_MEM [101:70] <= Shifted_wire + ID_EX [142:111];
+	EX_MEM [106:102] <= ID_EX [151:147];
+end
+mux4x1_32bit mux_1(ID_EX [110:79],WRITE_BACK_DATA,ALU_OUT,0,MUX_RS,mux1_out);
+mux4x1_32bit mux_2(ID_EX [78:47],WRITE_BACK_DATA,ALU_OUT,0,MUX_RT,mux2_out);
+forwarding_unit(ID_EX [14:10],ID_EX [9:5],RD_EX,RD_MEM,WB_EX,WB_MEM,MUX_RS,MUX_RT);
+ALUControl ALUControl1(ID_EX [145:144],ID_EX [20:15],ALUCtrl);
+ALU ALU1(mux1_out,((ID_EX [143])? ID_EX [46:15]:mux2_out),ALUCtrl,ALUOut,Zero_wire);
+ShiftLeft2 ShiftLeft2_1 (ID_EX [46:15],Shifted_wire);
+endmodule
+
+
+module MemoryStageHAZARD(MEM_WB,PCSrc,BranchAddress,clk,EX_MEM,load,Data);
+output reg [70:0] MEM_WB;
+output reg PCSrc;
+output reg [31:0] BranchAddress;
+input clk,load;
+input [7:0] Data;
+input [106:0] EX_MEM;
+wire [31:0] READ_DATA;
+
+always@(posedge clk)
+begin
+	PCSrc <= EX_MEM [69] & EX_MEM [104];
+	BranchAddress <= EX_MEM [101:70];
+end
+
+always@(negedge clk)
+begin
+	MEM_WB [4:0] <= EX_MEM[4:0];
+	MEM_WB [36:5] <= EX_MEM [68:37];
+	MEM_WB [68:37] <= READ_DATA;
+	MEM_WB [70:69] <= EX_MEM [106:105];
+end
+
+DataMemory DataMemory1(READ_DATA,EX_MEM [68:37],EX_MEM [36:5],EX_MEM [102],EX_MEM [103],clk,load,Data);
+endmodule
+
+
+module WBStageHAZARD(WriteRegister,WRITE_DATA,RegWrite,clk,MEM_WB);
+input [70:0] MEM_WB;
+input clk;
+output reg [4:0] WriteRegister;
+output reg [31:0] WRITE_DATA;
+output reg RegWrite;
+
+always@(posedge clk)
+begin
+	WriteRegister <= MEM_WB [4:0];
+	WRITE_DATA <= (MEM_WB [69])? MEM_WB [68:37]:MEM_WB [36:5];
+	RegWrite <= MEM_WB [70];
+end
+endmodule
+
+
+module PipelineMIPSHAZARD(clk,RST,loadRegFile,loadInstructionMem,loadDataMem,RegFileData,InstructionMemData,DataMemData);
+input clk,RST,loadRegFile,loadInstructionMem,loadDataMem;
+input [31:0] RegFileData ,InstructionMemData;
+input [7:0] DataMemData;
+
+wire PCSrc,REG_WRITE;
+wire [63:0]IF_ID;
+wire [151:0]ID_EX;
+wire [4:0] WRITE_REGISTER;
+wire [31:0] WRITE_DATA,BranchAddress;
+wire [106:0]EX_MEM;
+wire [70:0]MEM_WB;
+
+
+ExecutionStageHAZARD(EX_MEM,clk,ID_EX,EX_MEM[106],MEM_WB[70],EX_MEM[4:0],MEM_WB[4:0],EX_MEM[68:37],WRITE_DATA);
+FetchStageHAZARD FetchStage1(IF_ID,clk,PCSrc,BranchAddress,RST,loadInstructionMem,InstructionMemData);
+DecodeStageHAZARD DecodeStage1(ID_EX,IF_ID,clk,WRITE_REGISTER,WRITE_DATA,REG_WRITE,RST,loadRegFile,RegFileData);
+//ExecutionStageHAZARD ExecutionStage1(EX_MEM,clk,ID_EX);
+MemoryStageHAZARD MemoryStage1(MEM_WB,PCSrc,BranchAddress,clk,EX_MEM,loadDataMem,DataMemData);
+WBStageHAZARD WBStage1(WRITE_REGISTER,WRITE_DATA,REG_WRITE,clk,MEM_WB);
+
+endmodule
+
+
+//=======================tb==============//
+module tb_PipelineMIPSHAZARD();
+reg clk ,RST,loadRegFile,loadInstructionMem,loadDataMem;
+reg [31:0] RegFileData,InstructionMemData;
+reg [7:0] DataMemData;
+reg [31:0] RegFileDataFile [0:31];
+reg [31:0] InstructionFileDataFile [0:131071];
+integer i,j;
+
+initial
+begin
+clk = 1'b0;
+loadRegFile <= 0;
+loadInstructionMem <= 0;
+RST <= 1;
+$readmemb("F:/RegFileData.txt" , RegFileDataFile);
+$readmemb("F:/InstructionFileData.txt" , InstructionFileDataFile);
+#15
+fork
+begin
+	for (i = 0 ; i < 32; i = i + 1)
+	begin
+	#10
+	loadRegFile <= 1;
+	RegFileData <= RegFileDataFile[i];
+	end
+	#10
+	loadRegFile <= 0;
+end
+begin
+	for (j = 0 ; j < 200; j = j + 1)
+	begin
+	#10
+	loadInstructionMem <= 1;
+	InstructionMemData <= InstructionFileDataFile[j];
+	$display("%b \n",InstructionFileDataFile[j - 1]);
+	end
+	#10
+	loadInstructionMem <= 0;
+end
+join
+#5
+RST <= 0;
+
+end
+
+
+always
+begin
+#5
+clk = ~clk;
+end
+PipelineMIPSHAZARD PipelineMIPS1(clk,RST,loadRegFile,loadInstructionMem,loadDataMem,RegFileData,InstructionMemData,DataMemData);
+endmodule
+
+
+
+
+module tb_DECStageHAZARD();
+wire [146:0] ID_EX;
+reg [4:0] WRITE_REGISTER;
+reg [31:0] WRITE_DATA,Data;
+reg [63:0] IF_ID;
+reg REG_WRITE,clk,RST,load;
+reg[31:0] i;
+initial
+begin
+clk=1;
+load=1;
+RST=1;
+REG_WRITE=0;
+    for(i=0;i<10;i=i+1)
+    begin
+    #5
+    Data=10;
+    
+    end
+    
+#5
+RST=0;
+load=0;
+REG_WRITE=1;
+WRITE_DATA=50;
+WRITE_REGISTER=19;
+IF_ID=32'h0000_0000;
+
+    
+end
+
+always
+begin
+#5
+clk = ~clk;
+end
+
+DecodeStageHAZARD DEC(ID_EX,IF_ID,clk,WRITE_REGISTER,WRITE_DATA,REG_WRITE,RST,load,Data);
+endmodule
+
+
+module tb_registerfileHAZARD(); 
+reg [4:0] READ_REGISTER2,READ_REGISTER1,WRITE_REGISTER;
+reg REG_WRITE,load,clk,RST;
+reg [31:0] WRITE_DATA,Data;
+wire [31:0] READ_DATA2,READ_DATA1;
+reg [31:0]i;
+initial
+begin
+clk=1;
+load=1;
+RST=1;
+REG_WRITE=0;
+   for(i=0;i<10;i=i+1)
+    begin
+    #5
+    Data=i;
+    
+    end
+    
+#5
+RST=0;
+load=0;
+READ_REGISTER1=0;
+READ_REGISTER2=1;   
+REG_WRITE=1;
+WRITE_REGISTER=12;  
+WRITE_DATA=40;  
+end
+
+always
+begin
+#5
+clk = ~clk;
+end
+
+
+RegisterFile rf1(READ_DATA2,READ_DATA1,READ_REGISTER1,READ_REGISTER2,WRITE_REGISTER,WRITE_DATA,REG_WRITE,clk,RST,load,Data);
+endmodule
+
+
